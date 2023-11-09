65de39b8864cc384b630ae2647f174f03a9cc19a
Diego Verdeguer <dverdeguerv@gmail.com>
2023-11-08 23:51:04 -0500

    actualizando clase java

--- src/main/java/InformesGestionarSolicitud/Tests/C_testFlujoIntranetDireccion.java
+++ src/main/java/InformesGestionarSolicitud/Tests/C_testFlujoIntranetDireccion.java
@@ -50,7 +50,8 @@ public class C_testFlujoIntranetDireccion {
         intranetPage.setearCodigoTramite();
         intranetPage.seleccionarTramite();
 
-        //prueba gitit status
+        //prueba gitit statusgit
+        //asdasgit
 
     }
 }
